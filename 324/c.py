fdc8eec4218b41522adc93d017e37be2b73dc7d1
naoya25 <onaoya2002@gmail.com>
2023-10-16 12:15:24 +0900

    fix

--- 324/c.py
+++ 324/c.py
@@ -1,34 +1,23 @@
-def remove_s_index(s, i):
-    if 0 <= i < len(s):
-        result = s[:i] + s[i + 1:]
-        return result
-
+# TLE
 def main(n, t1, lent1):
     similars = []
     for i in range(n):
-        s = input()
+        s = input().strip()
         lens = len(s)
 
         if s == t1:
             similars.append(i + 1)
         elif lens == lent1:
-            diff_count = 0
-            for s1, s2 in zip(s, t1):
-                if s1 != s2:
-                    diff_count += 1
-                    if diff_count > 1:
-                        break
+            diff_count = sum(1 for s1, s2 in zip(s, t1) if s1 != s2)
             if diff_count <= 1:
                 similars.append(i + 1)
         elif abs(lens - lent1) == 1:
             if lens > lent1:
-                s1, s2 = s, t1 # s1が大きい方
+                s1, s2 = s, t1
             else:
                 s1, s2 = t1, s
-            for s_i in range(len(s1)):
-                if remove_s_index(s1, s_i) == s2:
-                    similars.append(i + 1)
-                    break
+            if any(s1[:s_i] + s1[s_i + 1:] == s2 for s_i in range(len(s1))):
+                similars.append(i + 1)
     return similars
 
 if __name__ == '__main__':
