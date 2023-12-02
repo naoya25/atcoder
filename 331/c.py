e78b25e450b0bff341ffa18b4a23459883adea6b
naoya25 <onaoya2002@gmail.com>
2023-12-03 03:16:29 +0900

    fix 331 c

--- 331/c.py
+++ 331/c.py
@@ -1,4 +1,11 @@
-n = int(input())
-a_list = list(map(int, input().split()))
+# n = int(input())
+# a_list = list(map(int, input().split()))
 
-print(*[sum(e for e in a_list if e > a) for a in a_list])
+# print(*[sum(e for e in a_list if e > a) for a in a_list])
+
+import numpy as np
+
+n = int(input())
+a_list = np.array(list(map(int, input().split())))
+
+print(*[np.sum(a_list * (a_list > a)) for a in a_list])
