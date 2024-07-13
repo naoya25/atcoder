689ec1e29bc9668afa90d8a1f55262b4e346c298
naoya25 <onaoya2002@gmail.com>
2024-07-14 05:49:17 +0900

    ダイクストラ修正

--- アルゴリズム/dijkstra.py
+++ アルゴリズム/dijkstra.py
@@ -3,38 +3,35 @@ import heapq
 
 def main():
     n, m = map(int, input().split())
-    a = list(map(int, input().split()))
-    graph = [[] for _ in range(n + 1)]
+    a = list(map(int, input().split()))  # 頂点の重み
+    graph = [[] for _ in range(n)]  # 各頂点から繋がってる頂点と辺の重み
     for _ in range(m):
         u, v, b = map(int, input().split())
-        graph[u].append((v, b))
-        graph[v].append((u, b))
+        graph[u - 1].append((v - 1, b))
+        graph[v - 1].append((u - 1, b))
+    # print(graph)
 
-    def dijkstra(start):
-        min_heap = [(a[start - 1], start)]
-        dist = [float("inf")] * (n + 1)
-        dist[start] = a[start - 1]
-        visited = [False] * (n + 1)
+    # ダイクストラ
+    min_heap = [(a[0], 0)]  # 未確定の候補地(最小重み(未確定ではあるが),現在の頂点)
+    dist = [float("inf")] * n  # 頂点ごとの最短経路
+    dist[0] = a[0]
+    visited = [False] * n  # 確定した頂点
 
-        while min_heap:
-            current_dist, u = heapq.heappop(min_heap)
+    while min_heap:
+        nd, u = heapq.heappop(min_heap) # 重みが最小のものを選ぶ(確定する)
 
-            if visited[u]:
-                continue
-            visited[u] = True
+        if visited[u]:
+            continue
+        visited[u] = True
 
-            for v, weight in graph[u]:
-                if not visited[v] and dist[v] > current_dist + weight + a[v - 1]:
-                    dist[v] = current_dist + weight + a[v - 1]
-                    heapq.heappush(min_heap, (dist[v], v))
+        # 繋がってる頂点全部試す
+        for v, w in graph[u]:
+            # より短ければ更新
+            if not visited[v] and dist[v] > nd + w + a[v]:
+                dist[v] = nd + w + a[v]
+                heapq.heappush(min_heap, (dist[v], v))
 
-        return dist
-
-    shortest_paths = dijkstra(1)
-
-    ans = [shortest_paths[i] for i in range(2, n + 1)]
-    print(*ans)
-
+    print(*dist[1:])
     return
 
 
