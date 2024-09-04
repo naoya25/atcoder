6ba5cdd7c4337ab1f543b7acf7aca774d8fded5b
naoya25 <onaoya2002@gmail.com>
2024-09-05 00:37:32 +0900

    2別解

--- 競プロ典型/2.py
+++ 競プロ典型/2.py
@@ -1,35 +1,64 @@
+# def main():
+#     n = int(input())
+#     if n % 2 == 1:
+#         return
+#     s1, s2 = "(", ")"
+
+#     def judge(bit):
+#         cnt = 0
+#         s = ""
+#         for i in range(n):
+#             if bit & (1 << i):
+#                 cnt += 1
+#                 s += s1
+#             else:
+#                 if cnt < 1:
+#                     return False, ""
+#                 cnt -= 1
+#                 s += s2
+#         if cnt == 0:
+#             return True, s
+#         else:
+#             return False, ""
+
+#     ans = []
+#     for bit in range(1 << n):
+#         j, s = judge(bit)
+#         if j:
+#             ans.append(s)
+
+#     ans.sort()
+#     for r in ans:
+#         print(r)
+#     return
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+from itertools import product
+
+
 def main():
     n = int(input())
     if n % 2 == 1:
         return
-    s1, s2 = "(", ")"
 
-    def judge(bit):
+    def judge(s):
         cnt = 0
-        s = ""
-        for i in range(n):
-            if bit & (1 << i):
+        for char in s:
+            if char == "(":
                 cnt += 1
-                s += s1
             else:
                 if cnt < 1:
-                    return False, ""
+                    return False
                 cnt -= 1
-                s += s2
-        if cnt == 0:
-            return True, s
-        else:
-            return False, ""
+        return cnt == 0
 
-    ans = []
-    for bit in range(1 << n):
-        j, s = judge(bit)
-        if j:
-            ans.append(s)
-
-    ans.sort()
-    for r in ans:
-        print(r)
+    for s in product(["(", ")"], repeat=n):
+        if judge(s):
+            print("".join(s))
     return
 
 
