1bcc670bd5515199fc1ac51836a90c7130277676
naoya25 <onaoya2002@gmail.com>
2025-04-12 20:39:04 +0900

    log 2025-04-12 20:39:04

---
+++ ABC/400_dart/b.dart
@@ -0,0 +1,107 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
+void main() {
+  final input = Input();
+  final [n, m] = input.getInts(2);
+
+  int x = 0;
+
+  for (int i = 0; i <= m; i++) {
+    x += pow(n, i).toInt();
+    if (x > pow(10, 9)) {
+      print('inf');
+      return;
+    }
+  }
+
+  if (x > pow(10, 9)) {
+    print('inf');
+  } else {
+    print(x);
+  }
+}
+
+class Input {
+  static Input? _instance;
+  Input._() : data = File('/dev/stdin').readAsStringSync(encoding: utf8);
+
+  factory Input() {
+    _instance ??= Input._();
+    return _instance!;
+  }
+
+  final String data;
+  int pos = 0;
+
+  bool isSpace(String ch) {
+    return ch == " " || ch == "\n";
+  }
+
+  String getString() {
+    var p = pos;
+    while (p < data.length && isSpace(data[p])) p++;
+    final from = p;
+    while (p < data.length && !isSpace(data[p])) p++;
+    final to = p;
+    pos = p;
+    return data.substring(from, to);
+  }
+
+  List<String> getStringAsList() {
+    return getString().split('');
+  }
+
+  List<String> getStrings(int n) {
+    final result = <String>[];
+    for (int i = 0; i < n; ++i) {
+      result.add(getString());
+    }
+    return result;
+  }
+
+  int getInt() {
+    return int.parse(getString());
+  }
+
+  int getInt0idx() {
+    return getInt() - 1;
+  }
+
+  List<int> getInts(int n) {
+    final result = <int>[];
+    for (int i = 0; i < n; ++i) {
+      result.add(getInt());
+    }
+    return result;
+  }
+
+  List<int> getInts0idx(int n) {
+    final result = <int>[];
+    for (int i = 0; i < n; ++i) {
+      result.add(getInt0idx());
+    }
+    return result;
+  }
+
+  double getDouble() {
+    return double.parse(getString());
+  }
+
+  List<List<int>> getInt2d(int n, int m) {
+    final result = <List<int>>[];
+    for (int i = 0; i < n; ++i) {
+      result.add(getInts(m));
+    }
+    return result;
+  }
+}
+
+void printBool(bool b) {
+  print(b ? 'Yes' : 'No');
+}
+
+void printList(List list, {String separator = ' '}) {
+  print(list.join(separator));
+}
